76b63ad0141574508c4c7774d2c87227c1948b81
Shirogane Tsumugi <inapp@iccmc.cc>
2025-12-18 02:49:21 +0800

    move st7701 initalization to kernel. revert u-boot to no-st7701 init state.

--- flashsystem.sh
+++ flashsystem.sh
@@ -1,6 +1,6 @@
 xfel spinand erase 0 0x8000000
 xfel spinand splwrite 1024 0 output/images/u-boot-sunxi-with-spl.bin
 xfel spinand write 0x20000 output/images/u-boot.img
-xfel spinand write 0x100000 output/images/devicetree.dtb
+xfel spinand write 0x100000 output/images/devicetree-0.3.dtb
 xfel spinand write 0x120000 output/images/zImage
 xfel spinand write 0x680000 output/images/ubi.img
